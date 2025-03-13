008b211aa99e2113ebb3fef56288367f55f6e3e0
lZh1r <electroxhackerpig09@gmail.com>
2025-03-13 22:16:45 +0300

    astolfo easter egg

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
 const audio = new Audio('assets/headshot.mp3');
 
+let count = 0;
+
 // TODO: Replace destination
 document.getElementById('pillButton').addEventListener("click", function() {
-  window.location.href = "/console";
+  window.location.href = "https://chat.femboymatrix.su/";
 });
 
 document.getElementById('astolfo').addEventListener("click", function() {
+  count++;
+  if (count === 3) {
+    window.location.href = "/console";
+    count = 0;
+  }
   audio.play();
 });
 
